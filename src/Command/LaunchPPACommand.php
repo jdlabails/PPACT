5abff3f0b145392503fdd329254f8346005f3383
jdlabails <jd.labails@gmail.com>
2018-04-29 09:30:54 +0200

    fix path sf2 vs sf3

--- src/Command/LaunchPPACommand.php
+++ src/Command/LaunchPPACommand.php
@@ -21,7 +21,7 @@ class LaunchPPACommand extends Command
     public function __construct($ppactParams, $projectDir, EntityManagerInterface $entityManager)
     {
         $this->params = $ppactParams;
-        $this->workspaceDir = $projectDir.'/workspace';
+        $this->workspaceDir = $projectDir . '/workspace';
         $this->workspaceDir = '/home/jd/Projets';
         $this->entityManager = $entityManager;
 
@@ -32,8 +32,7 @@ class LaunchPPACommand extends Command
     {
         $this
             ->setName('control-tower:launch-projects')
-            ->setDescription('Launch analysis of projects configrured.')
-        ;
+            ->setDescription('Launch analysis of projects configrured.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -54,25 +53,36 @@ class LaunchPPACommand extends Command
         $launchProcesses = [];
         $readProcesses = [];
         foreach ($this->params as $project => $params) {
-            $dirPath = $this->workspaceDir.'/'.$project;
+            $dirPath = $this->workspaceDir . '/' . $project;
 
-            $cmdLine = 'cd '.$dirPath . ' && git pull';
+            // @todo check if we are on master
+            $cmdLine = 'cd ' . $dirPath . ' && git pull';
             if (!is_dir($dirPath)) {
                 mkdir($dirPath, 0755);
-                $cmdLine = 'git clone '.$params['url'].' '.$dirPath;
-                $cmdLine.=' && cd '.$dirPath;
+                $cmdLine = 'git clone ' . $params['url'] . ' ' . $dirPath;
+                $cmdLine .= ' && cd ' . $dirPath;
             }
 
-            //todo detect sf 2 ou 3 for console path
-            $cmdLine.=' && composer install && php bin/console ppa:analyse:launch';
+            $cmdLine .= ' && composer install';
+
+            $launchCmd = 'php bin/console ppa:analyse:launch';
+            $readCmd = 'php bin/console ppa:analyse:read';
+            if (file_exists($dirPath . '/app/console')) {
+                $launchCmd = 'php app/console ppa:analyse:launch';
+                $readCmd = 'php app/console ppa:analyse:read';
+            }
+
+            $cmdLine .= ' && ' . $launchCmd;
+
+            $output->writeln('Command for ' . $project . ' : ' . $launchCmd, OutputInterface::VERBOSITY_VERBOSE);
 
             $process = new Process($cmdLine);
             $process->start();
 
-            $launchProcesses[$project]=$process;
-            $readProcesses[$project]=new Process('cd '.$dirPath . ' && php bin/console ppa:analyse:read');
+            $launchProcesses[$project] = $process;
+            $readProcesses[$project] = new Process('cd ' . $dirPath . ' && ' . $readCmd);
 
-            $output->writeln('Workspace setup and analysis of '.$project.' have begun');
+            $output->writeln('Workspace setup and analysis of ' . $project);
         }
 
         $output->writeln([
@@ -91,11 +101,11 @@ class LaunchPPACommand extends Command
 
                 // end of anaylisis, write it and start reading
                 $output->writeln([
-                    '<info>'.$project.' is built, waiting for the results...</info>',
+                    '<info>' . $project . ' is built, waiting for the results...</info>',
                     '',
                 ]);
                 $output->writeln([
-                    '<info>Status for '.$project.'</info>',
+                    '<info>Status for ' . $project . '</info>',
                     $process->getOutput(),
                     ''
                 ],
@@ -112,7 +122,7 @@ class LaunchPPACommand extends Command
 
                 $json = $process->getOutput();
                 $output->writeln([
-                    '<info>result of analysis for '.$project.'</info>',
+                    '<info>result of analysis for ' . $project . '</info>',
                     $json,
                     ''
                 ],
@@ -130,7 +140,7 @@ class LaunchPPACommand extends Command
                 $analysisAsArray = json_decode($json, true);
                 if (null === $analysisAsArray) {
                     unset($readProcesses[$project]);
-                    $output->writeln('<error>Analysis finished but not readable for : '.$project.'</error>');
+                    $output->writeln('<error>Analysis finished but not readable for : ' . $project . '</error>');
                     continue;
                 }
                 $analysis->setFromArray($analysisAsArray);
@@ -139,7 +149,7 @@ class LaunchPPACommand extends Command
                 $this->entityManager->flush();
 
                 unset($readProcesses[$project]);
-                $output->writeln('<info>Analysis finished and saved for : '.$project.'</info>');
+                $output->writeln('<info>Analysis finished and saved for : ' . $project . '</info>');
             }
         } while ($aProcessIsStillRunning);
     }
